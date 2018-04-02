b95678cfcc02b41289a0c5c8abadba35291ec697
Jean Arhancetebehere <jean.arhancetebehere@gmail.com>
2018-04-02 14:29:41 +0200

    initial commit

---
+++ src/test/java/com/miage/souvenirs/SouvenirsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.miage.souvenirs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SouvenirsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
