8855ff9e05c728adf9102c974a7860f166182346
jordanbazerque <ba-ba_@hotmail.fr>
2018-04-05 11:06:06 +0200

    add angular

---
+++ src/main/java/com/miage/souvenirs/SouvenirsApplication.java
@@ -0,0 +1,12 @@
+package com.miage.souvenirs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouvenirsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouvenirsApplication.class, args);
+	}
+}
